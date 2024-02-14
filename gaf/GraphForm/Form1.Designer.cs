4ba344cbfeef9b33d3442a5ba70175cfce44d152
Michal-Vrbicky-GitHub <144424827+Michal-Vrbicky-GitHub@users.noreply.github.com>
2024-02-14 15:58:42 +0100

    as
    fg

---
+++ gaf/GraphForm/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace GraphForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportAsPNGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uzToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.monthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valuesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.valuesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.printujTyJoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.valuesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valuesBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.yearDataGridViewTextBoxColumn,
+            this.monthDataGridViewTextBoxColumn,
+            this.valueDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.valuesBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(229, 318);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(247, 27);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(541, 318);
+            this.cartesianChart1.TabIndex = 3;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveAsToolStripMenuItem.Image")));
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportAsPNGToolStripMenuItem,
+            this.uzToolStripMenuItem,
+            this.printujTyJoToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // exportAsPNGToolStripMenuItem
+            // 
+            this.exportAsPNGToolStripMenuItem.Name = "exportAsPNGToolStripMenuItem";
+            this.exportAsPNGToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportAsPNGToolStripMenuItem.Text = "Export as PNG";
+            this.exportAsPNGToolStripMenuItem.Click += new System.EventHandler(this.exportAsPNGToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // uzToolStripMenuItem
+            // 
+            this.uzToolStripMenuItem.Name = "uzToolStripMenuItem";
+            this.uzToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uzToolStripMenuItem.Text = "uz";
+            this.uzToolStripMenuItem.Click += new System.EventHandler(this.uzToolStripMenuItem_Click);
+            // 
+            // yearDataGridViewTextBoxColumn
+            // 
+            this.yearDataGridViewTextBoxColumn.DataPropertyName = "Year";
+            this.yearDataGridViewTextBoxColumn.FillWeight = 42F;
+            this.yearDataGridViewTextBoxColumn.HeaderText = "Year";
+            this.yearDataGridViewTextBoxColumn.Name = "yearDataGridViewTextBoxColumn";
+            this.yearDataGridViewTextBoxColumn.Width = 42;
+            // 
+            // monthDataGridViewTextBoxColumn
+            // 
+            this.monthDataGridViewTextBoxColumn.DataPropertyName = "Month";
+            this.monthDataGridViewTextBoxColumn.FillWeight = 42F;
+            this.monthDataGridViewTextBoxColumn.HeaderText = "Month";
+            this.monthDataGridViewTextBoxColumn.Name = "monthDataGridViewTextBoxColumn";
+            this.monthDataGridViewTextBoxColumn.Width = 42;
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            this.valueDataGridViewTextBoxColumn.DataPropertyName = "Value";
+            this.valueDataGridViewTextBoxColumn.HeaderText = "Value";
+            this.valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            // 
+            // valuesBindingSource
+            // 
+            this.valuesBindingSource.DataSource = typeof(GraphForm.Values);
+            // 
+            // valuesBindingSource1
+            // 
+            this.valuesBindingSource1.DataSource = typeof(GraphForm.Values);
+            // 
+            // printujTyJoToolStripMenuItem
+            // 
+            this.printujTyJoToolStripMenuItem.Name = "printujTyJoToolStripMenuItem";
+            this.printujTyJoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.printujTyJoToolStripMenuItem.Text = "printuj ty jo";
+            this.printujTyJoToolStripMenuItem.Click += new System.EventHandler(this.printujTyJoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 355);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.valuesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valuesBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource valuesBindingSource;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.BindingSource valuesBindingSource1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportAsPNGToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn monthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ToolStripMenuItem uzToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printujTyJoToolStripMenuItem;
+    }
+}
+
---
+++ gaf/GraphForm/Form1.cs
@@ -0,0 +1,172 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Wpf.Charts.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GraphForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool uz = false; Bitmap memoryImage;
+
+        public void UpdateGraph()
+        {
+            cartesianChart1.Series.Clear();
+            LiveCharts.SeriesCollection sc = new LiveCharts.SeriesCollection();
+
+            //if (valuesBindingSource.DataSource != null) { 
+            if (valuesBindingSource.DataSource.ToString()!=/*"{Name = \"Values\" FullName = \"GraphForm.Values\"}"*/"{Method = {System.String ToString()}}"){
+                var years = (from o in valuesBindingSource.DataSource as List<Values> select new { Year = o.Year }).Distinct();
+
+                foreach (var y in years)
+                {
+                    List<double> values = new List<double>();
+
+                    for (int month = 1; month <= 12; month++)
+                    {
+                        double val = 0;
+
+                        var data = from o in valuesBindingSource.DataSource as List<Values>
+                                   where o.Year.Equals(y.Year) && o.Month.Equals(month)
+                                   orderby o.Month ascending
+                                   select new { o.Value, o.Month };
+
+                        // Sum the values for the same month
+                        foreach (var item in data)
+                        {
+                            val += item.Value;
+                        }
+
+                        values.Add(val);
+                    }
+
+                    sc.Add(new LineSeries()
+                    {
+                        Title = y.Year.ToString(),
+                        Values = new ChartValues<double>(values)
+                    });
+                }
+
+                cartesianChart1.Series = sc;
+            }
+        }
+
+
+        private void pieChart1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            valuesBindingSource.DataSource = new List<Values>();
+
+            cartesianChart1.AxisX.Add(new LiveCharts.Wpf.Axis
+            {
+                Title = "Month",
+            });
+            cartesianChart1.AxisY.Add(new LiveCharts.Wpf.Axis
+            {
+                Title = "Value",
+            });
+
+            cartesianChart1.LegendLocation = LiveCharts.LegendLocation.Right;
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (valuesBindingSource.DataSource == null)
+            {
+                return;
+            }
+            if(uz)
+                UpdateGraph();
+        }
+
+        private void exportAsPNGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //using(var bitmap = new Bitmap(cartesianChart1 ))
+            var chart = cartesianChart1;
+
+            using (var bmp = new Bitmap(chart.Width, chart.Height))
+            {
+                chart.DrawToBitmap(bmp, new Rectangle(0, 0, chart.Width, chart.Height));
+                bmp.Save("screenshotchart1.png");
+            }
+            //this.cartesianChart1.SaveImage("C:\\mycode\\mychart.png", ChartImageFormat.Png);
+        }
+
+        private void uzToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            uz = true;
+        }
+
+        private void printujTyJoToolStripMenuItem_Click(object sender, EventArgs e)
+        {/*
+            var pDq = new PrintDialog();
+            var pDoc = new PrintDocument   ();
+            pDoc.PrintPage += new PrintPageEventHandler(pr)*//*
+            Graphics myGraphics = this.CreateGraphics();
+            Size s = this.Size;
+            memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+            memoryGraphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
+
+            printDocument1.Print();*/
+
+            var pDq = new PrintDialog();
+            var pDoc = new PrintDocument();
+            pDoc.PrintPage += new PrintPageEventHandler(PpPppp); 
+
+            if(pDq.ShowDialog() == DialogResult.OK)
+            {
+                pDoc.Print();
+            }
+        }
+
+
+        private void PrintDocument1_PrintPage(System.Object sender,
+           System.Drawing.Printing.PrintPageEventArgs e) =>
+               e.Graphics.DrawImage(memoryImage, 0, 0);
+
+        void PpPppp(object sender, PrintPageEventHandler e)
+        {
+            var chart = cartesianChart1;
+            var bmp = new Bitmap(chart.Width, chart.Height);
+            chart.DrawToBitmap(bmp, new Rectangle(0, 0, chart.Width, chart.Height));
+            //e.gra
+        }
+
+        void PpPppp(object sender, PrintPageEventArgs e)
+        {
+            var chart = cartesianChart1;
+            var bmp = new Bitmap(chart.Width, chart.Height);
+            chart.DrawToBitmap(bmp, new Rectangle(0, 0, chart.Width, chart.Height));
+            //e.gra
+        }
+        /*
+        void PpPppp(object sender, PrintPageEventArgs e)
+        {
+            var chart = cartesianChart1;
+            var bmp = new Bitmap(chart.Width, chart.Height);
+            chart.DrawToBitmap(bmp, new Rectangle(0, 0, chart.Width, chart.Height));
+            //e.gra
+        }*/
+
+    }
+}
